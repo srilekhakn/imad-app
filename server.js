eef887c5b130143e2da153dd2eb022500ae88951
srilekhakn <srilekhakn27@gmail.com>
2017-08-17 11:58:03 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,94 +5,85 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-var articals={
-articalOne : {
-    title: 'Artical one|Srilekha KN',
-    header: 'Artical one',
-    date: '13th Aug 2017',
-    content:`<p>
-                   This is the content for my first artical. This is the content for my first artical. This is the content for my first artical. This is the content for my first artical. This is the content for my first artical. This is the content for my first artical. This is the content for my first artical. This is the content for my first artical. This is the content for my first artical. This is the content for my first artical.
-               </p>
-               
-               <p>
-                   This is the content for my first artical. This is the content for my first artical. This is the content for my first artical. This is the content for my first artical. This is the content for my first artical. This is the content for my first artical. This is the content for my first artical. This is the content for my first artical. This is the content for my first artical. This is the content for my first artical.
-               </p>
-               
-               <p>
-                   This is the content for my first artical. This is the content for my first artical. This is the content for my first artical. This is the content for my first artical. This is the content for my first artical. This is the content for my first artical. This is the content for my first artical. This is the content for my first artical. This is the content for my first artical. This is the content for my first artical.
-               </p>`
-    
-        },
-articalTwo:{
-     title: 'Artical Two|Srilekha KN',
-    header: 'Artical Two',
-    date: '14th Aug 2017',
-    content:`<p>
-                   This is the content for my secound artical.  
-               </p>`
-        },
-articalThree:{
-    title: 'Artical Three|Srilekha KN',
-    header: 'Artical Three',
-    date: '15th Aug 2017',
-    content:`<p>
-                   This is the content for my third artical.  
-               </p>`
-    
-    }
+var articals= {
+	'artical-one': {
+
+	    title: 'Artical one|Srilekha KN',
+	    heading: 'Artical one',
+	    date: '13th Aug 2017',
+	    content:"<p> This is my first artical </p>"
+	},
+	'artical-two': {
+
+	    title: 'Artical two|Srilekha KN',
+	    heading: 'Artical two',
+	    date: '13th Aug 2017',
+	    content:"<p> This is my secound artical </p>"
+	},
+	'artical-three': {
+
+	    title: 'Artical three|Srilekha KN',
+	    heading: 'Artical three',
+	    date: '13th Aug 2017',
+	    content:"<p> This is my third artical </p>"
+	}
+
 };
 
+
 function createTemplate(data){
-    var title=data.title;
-    var header=data.header;
+    var title = data.title;
+    var heading=data.heading;
     var date=data.date;
     var content=data.content;
+
     
-var htmlTemplate=`
-<html>
-<head>
-    <title>${title}</title>
-    <meta name="viewport" content="width=device-width, initalscale=1" />
-    <link href="/ui/style.css" rel="stylesheet"/>
-</head>
-<body>
-    <div class="container">
-            <div> 
-                <a href="/">HOME</a>
+   var htmlTemplate=`
+    <html>
+    <head>
+        <title>
+            ${title}
+        </title>
+        <meta name="viewport" content="width-device-width, initial-scale=1" />
+        <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    <body>
+        <div class="container">
+            <div>
+                <a href="/">Home</a>
             </div>
-            
-           <hr/>
-           
-           <h3>
-               ${header}
-           </h3>
-           
-           <div>
-               ${date}
-           </div>
-           
-           <div>
-               ${content}
-           </div>
-   </div>
-</body>
-</html> `;
+            <hr/>
+            <h3>
+               ${heading}
+            </h3>
+            <div>
+                ${date}
+            </div>
+            <div>
+                ${content}
+            </div>
+        </div>
+    </body>
+</html>`;
 
 return htmlTemplate;
-
 }
+
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/:articalName', function (req, res) {
-    var articalName= req.params.articalName;
+  var articalName=req.params.articalName;
   res.send(createTemplate(articals[articalName]));
 });
 
 
 app.get('/ui/style.css', function (req, res) {
+  res.sendFile(path.join(__dirname,'ui','style.css'));
+});
+app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname,'ui','madi.png'));
 });
 // Do not change port, otherwise your app won't run on IMAD servers
