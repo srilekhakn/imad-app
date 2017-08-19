08a045c36ab5afc29ab88818a5be661f84942a9a
srilekhakn <srilekhakn27@gmail.com>
2017-08-19 16:04:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -69,19 +69,33 @@ function createTemplate(data){
 return htmlTemplate;
 }
 
-
+var names=[];
+app.get('/submit-name/',function(req,res){
+	var name=req.query.name;
+	names.push(name);
+	res.send(JSON.stringify(names));
+});
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var counter=0;
+app.get('/counter',function(req,res){
+ 	counter=counter+1;
+	res.send(counter.toString());
+});
 app.get('/:articalName', function (req, res) {
   var articalName=req.params.articalName;
   res.send(createTemplate(articals[articalName]));
 });
 
 
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname,'ui','style.css'));
+});
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname,'ui','main.js'));
 });
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname,'ui','madi.png'));
@@ -89,7 +103,7 @@ app.get('/ui/madi.png', function (req, res) {
 // Do not change port, otherwise your app won't run on IMAD servers
 // Use 8080 only for local development if you already have apache running on 80
 
-var port = 80;
+var port = 8080;
 app.listen(port, function () {
   console.log(`IMAD course app listening on port ${port}!`);
 });
