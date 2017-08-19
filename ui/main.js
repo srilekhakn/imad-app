370231fbd5b4fee33667a0be5ca79bd40814d833
srilekhakn <srilekhakn27@gmail.com>
2017-08-19 16:07:02 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,55 @@
-console.log('Loaded!');
+var button=document.getElementById('counter');
+var counter=0;
+button.onclick = function(){
+	var request= new XMLHttpRequest();
+	//create the request
+	request.onreadystatechange = function(){
+	if(request.readyState===XMLHttpRequest.DONE){
+	if(request.status===200){
+		var counter = request.responseText;
+		var span=document.getElementById('count');
+		span.innerHTML = counter.toString();
+	}
+}
+
+	};
+	//make the request
+	request.open('GET','http://localhost:8080/counter',true);
+	request.send(null);
+	
+};
+
+var submit = document.getElementById('submit_btn');
+submit.onclick = function(){
+	
+	
+	
+	var request= new XMLHttpRequest();
+	//create the request
+	request.onreadystatechange = function(){
+	if(request.readyState===XMLHttpRequest.DONE){
+	if(request.status===200){
+
+		var names=request.responseText;
+	names=JSON.parse(names);
+	var list='';
+	for(var i=0;i<names.length;i++)
+	{
+	list +='<li>' + names[i] +'</li>'
+	}
+	var ul=document.getElementById('namelist');
+	ul.innerHTML=list;
+	}
+}
+
+	};
+	//make the request
+	var nameInput=document.getElementById('name');
+	var name=nameInput.value;
+	request.open('GET','http://localhost:8080/submit-name?name='+name,true);
+	request.send(null);
+
+
+	
+};
+	
