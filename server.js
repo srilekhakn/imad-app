eb710e10c214f77f7c44879574d5be7df5cd11d1
srilekhakn <srilekhakn27@gmail.com>
2017-08-22 11:26:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -110,7 +110,19 @@ app.get('/counter',function(req,res){
 
 app.get('/articles/:articalName', function (req, res) {
   var articalName=req.params.articalName;
-  res.send(createTemplate(articals[articalName]));
+  pool.query("SELECT * FROM article where title =" + req.params.articleName,function(err,result){
+     if(err){
+         res.status(500).send(err.toString());
+     }else{
+         if(result.rows.length===0){
+             res.status(404).send('Article not found');
+         }else{
+             var articleData = result.rows[0];
+             res.send(createTemplate(articleData));
+         }
+     }
+      
+  });
 });
 
 
