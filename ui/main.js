ae3104fcb1c3cb07ab0cf510ae84356888cb4859
srilekhakn <srilekhakn27@gmail.com>
2017-08-23 13:54:54 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,8 +9,6 @@ submit.onclick = function(){
 	request.onreadystatechange = function(){
 	if(request.readyState===XMLHttpRequest.DONE){
 	if(request.status===200){
-
-	console.log('user logged in');
 	alert('logged in successfully');
 	}else if(request.status === 403){
 	    alert('username/password is incorrect');
