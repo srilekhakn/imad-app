7e5496a106e9c48efd2ca8a47733b1f098e94336
srilekhakn <srilekhakn27@gmail.com>
2017-08-23 13:42:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -27,6 +27,7 @@ submit.onclick = function(){
 	console.log(username);
 	console.log(password);
 	request.open('POST','http://srilekhakn27.imad.hasura-app.io/login',true);
+	request.setRequestHeader('Content-Type','application/json');
 	request.send(JSON.stringify({username: username, password: password}));
 
 
