b4c15cc2c59e7a06eca53acb0a26a999d2a1d12b
srilekhakn <srilekhakn27@gmail.com>
2017-08-23 13:40:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -10,17 +10,15 @@ submit.onclick = function(){
 	if(request.readyState===XMLHttpRequest.DONE){
 	if(request.status===200){
 
-		var names=request.responseText;
-	names=JSON.parse(names);
-	var list='';
-	for(var i=0;i<names.length;i++)
-	{
-	list +='<li>' + names[i] +'</li>';
+	console.log('user logged in');
+	alert('logged in successfully');
+	}else if(request.status === 403){
+	    alert('username/password is incorrect');
+	}else if(request.status===500){
+	    alert('Something went wrong on the server');
 	}
-	var ul=document.getElementById('namelist');
-	ul.innerHTML=list;
-	}
-}
+	
+    }
 
 	};
 	//make the request
@@ -28,8 +26,8 @@ submit.onclick = function(){
 	var password=document.getElementById('password').value;
 	console.log(username);
 	console.log(password);
-	request.open('POST','http://srilekhakn27.imad.hasura-app.io/submit-name?name='+name,true);
-	request.send(JSON.strinfigy({username: username, password: password}));
+	request.open('POST','http://srilekhakn27.imad.hasura-app.io/login',true);
+	request.send(JSON.stringify({username: username, password: password}));
 
 
 	
