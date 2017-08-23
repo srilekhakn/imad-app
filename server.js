fcdb11eb20878af302a9e965477d7a089a3e90c9
srilekhakn <srilekhakn27@gmail.com>
2017-08-23 11:36:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
 var crypto = require('crypto');
+var bodyParser = require('body-parser');
+
+
 var config = {
     user: 'srilekhakn27',
     database: 'srilekhakn27',
@@ -13,7 +16,7 @@ var config = {
 
 var app = express();
 app.use(morgan('combined'));
-
+app.use(bodyParser.json());
 
 var articals= {
 	'artical-one': {
@@ -53,6 +56,21 @@ app.get('/hash/:input',function(req,res){
     
 });
 
+app.post('/create-user',function(req,res){
+    
+    var username=req.body.username;
+    var password=req.body.password;
+    var salt = crypto.randomByTes(128).toString('hex');
+   var dbString=hash(password,salt); 
+   pool.query('INSERT INTO "user"(username,password) VALUES ($1,$2)',[username,dbString],function(err,result){
+       if(err){
+        res.status(500).send(err.toString());
+        }
+        else{
+            res.send('User succesfully created:'+username);
+        }
+   });
+});
 var pool=new Pool(config);
 app.get('/test-db',function(req,res){
     pool.query('select *from test', function(err,result) {
